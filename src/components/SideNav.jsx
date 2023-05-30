eb1ce842242a7bc8b0e56e7266e9a736cb2853ec
Chandrubruce7 <134265664+Chandrubruce7@users.noreply.github.com>
2023-05-31 01:46:01 +0530

    sidenav is inprogress

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,34 +1,83 @@
-import React from 'react';
-
+import { React,useState } from 'react'
+import { Link,NavLink } from 'react-router-dom';
 import { userhovtab,usertab,hearthovsvg,heart,subtab,subtabhov,
     helpcircletab,helpcirclehovtab,termsandconditionstab,termsandconditionstabhov,shieldtab,shieldhovtab,logouttab
    } from '../components/images/images.jsx';
-const SideNav = () => {
+const SideNav = ({children}) => {
     
+    const[isOpen ,setIsOpen] = useState(false);
+    const toggle = () => setIsOpen (!isOpen);
+
+    const generalItem=[
+        {
+            path:"/ProfilePage",
+            name:"Profile",
+            img:usertab,
+            img1:userhovtab
+        },
+        {
+            path:"/Favorites",
+            name:"Favorites",
+            img:heart,
+            img1:hearthovsvg
+        },
+        {
+            path:"/GoandFreeNow",
+            name:"Go and Free Now",
+            img:subtab,
+            img1:subtabhov
+        },
+        
+    ]
+
+    const otherItem=[
+        {
+            path:"/ProfilePage",
+            name:"Profile",
+            img:usertab,
+            img1:userhovtab
+        },
+        {
+            path:"/Favorites",
+            name:"Favorites",
+            img:heart,
+            img1:hearthovsvg
+        },
+        {
+            path:"/GoandFreeNow",
+            name:"Go and Free Now",
+            img:subtab,
+            img1:subtabhov
+        },
+        
+    ]
+
+
     return (
     
              <div className="col-lg-3 col-md-12 col-sm-12 tab-navbar p-0">
                    <h1 className="font-28">Quick Links</h1>
                    <div className="tab-list nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
                        <h6 className="font-18">General</h6>
-                       <button className="font-18 nav-tab-btn " id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-fav" aria-selected="true">
-                        <img className="active-none" src={userhovtab} alt="" srcSet=""/>
-                        <img src={usertab} alt="" srcSet=""/>
-                        <span>Profile</span>
-                        <a className="mbl-block" href="./Edit-profile.html">Profile</a>
-                    </button>
-                        <button className="font-18 nav-tab-btn " id="v-pills-fav-tab" data-bs-toggle="pill" data-bs-target="#v-pills-fav" type="button" role="tab" aria-controls="v-pills-fav" aria-selected="true">
-                            <img className="active-none" src={hearthovsvg} alt="" srcSet=""/>
-                            <img src={heart} alt="" srcSet=""/>
-                            <span>Favorites</span>
-                            <a className="mbl-block" href="./Edit-fav.html">Favorites</a>
+
+                       {
+                   generalItem.map((item, index)=>(
+                    <NavLink key={index} to={item.path} className="link" activeclassName="active">
+                        <button className="font-18 nav-tab-btn " activeclassName="show active" type="button">
+
+                        <img src={item.img} alt="" srcSet=""/>
+                        <img className="active-none" src={item.img1} alt="" srcSet=""/>
+                        <span>{item.name}</span>
                         </button>
-                        <button className="font-18 nav-tab-btn" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">                                                 
-                             <img src={subtab} alt="" srcSet=""/>
-                             <img className="active-none" src={subtabhov} alt="" srcSet=""/>
-                          <span>Go and Free Now</span>
-                          <a className="mbl-block" href="./Edit-gotofree.html">Go and Free Now</a>
-                        </button>
+                     </NavLink>
+                    //    <NavLink to={item.path} key={index} className="link" activeclassName="active">
+                    //        <div className="icon">{item.icon}</div>
+                    //        <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>
+                    //    </NavLink>
+                   ))
+               }
+
+
                         <h6 className="font-18">Other</h6>
                         <button className="font-18 nav-tab-btn" id="v-pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#v-pills-disabled" type="button" role="tab" aria-controls="v-pills-disabled" aria-selected="false" >                                                 
                           <img src={helpcircletab} alt="" srcSet=""/>  
